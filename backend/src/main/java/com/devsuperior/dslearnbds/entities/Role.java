7f0eaaccf23ee99ce1c5099d16d3673fc5314656
Samir Nagib <samir.nagib@gmail.com>
2022-11-16 20:48:13 -0300

    04-05 User, Role

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -1,18 +1,31 @@
 package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
 	
-public class Role {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	
+	public Role() {
+	}
 	
 	public Role(Long id, String authority) {
 		super();
 		this.id = id;
 		this.authority = authority;
-	}
-
-	private Long id;
-	private String authority;
-	
-	Public Role() {
-		
 	}
 
 	public Long getId() {
@@ -30,6 +43,32 @@ public class Role {
 	public void setAuthority(String authority) {
 		this.authority = authority;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 	
 	
 
