e8955b1fc47c104c538b7cf47e432b3a965910f1
Samir Nagib <samir.nagib@gmail.com>
2022-11-17 18:46:47 -0300

    Notification

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dslearnbds.entities;
+
+public class Section {
+	
+	private Long id;
+	private String title;
+	private String description;
+	private Integer position;
+	private String imgURi;
+
+}
