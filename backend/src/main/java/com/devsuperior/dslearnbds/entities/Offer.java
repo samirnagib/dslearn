c84dfe6b9934ab9ce02c3c214514f1d6621d70c8
Samir Nagib <samir.nagib@gmail.com>
2022-11-17 18:06:29 -0300

    04-08 Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +30,11 @@ public class Offer implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "course_id")
 	private Course course;
+	
+	
+	@OneToMany(mappedBy = "offer")
+	private List<Resource> resources = new ArrayList<>();
+	
 	
 	public Offer() {
 	}
@@ -102,6 +110,10 @@ public class Offer implements Serializable {
 		} else if (!Id.equals(other.Id))
 			return false;
 		return true;
+	}
+
+	public List<Resource> getResources() {
+		return resources;
 	}
 		
 	
