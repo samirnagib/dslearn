964971b39b828cac9482c320a3c3702d0295947f
Samir Nagib <samir.nagib@gmail.com>
2022-11-18 09:57:45 -0300

    04-19 Criando os repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/Lesson.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Lesson extends JpaRepository<Lesson, Long> {
+
+}
