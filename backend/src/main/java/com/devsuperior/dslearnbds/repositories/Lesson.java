97e75392aa5b87ba5135120bf2b19bf418aef294
Samir Nagib <samir.nagib@gmail.com>
2022-11-18 10:31:29 -0300

    04-20 Incluindo exceções, validação e segurança ao projeto

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/Lesson.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.dslearnbds.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Lesson extends JpaRepository<Lesson, Long> {
-
-}
